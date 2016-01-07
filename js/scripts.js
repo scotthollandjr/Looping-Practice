ad45a926992fadba5a187a5b97af587f44c9ce4e
Scott Holland <scotthollandjr@gmail.com>
2016-01-06 21:31:49 -0800

    Replace forEach loops with for loops

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
 
   var friends = ["Robert", "Jeff", "Adam", "Javier", "Tim", "Kevin"];
 
-  friends.forEach(function(friend) {
-    alert("Hello, " + friend);
-  });
+  for (var friendex = 0; friendex < friends.length; friendex += 1) {
+    alert("Hello, " + friends[friendex] + "!");
+  };
+
 
   var flavors = ["Peanut Butter Cup", "Horchata", "Butter Pecan", "Brownie", "Raspberry Cheesecake"];
 
   $("body").append("<h2>My favorite Ice Cream flavors are:");
-  flavors.forEach(function(flavor) {
-    $("body").append("<h3>" + flavor + "</h3>");
 
-
-  });
+  for ( var flavor = 0; flavor < flavors.length; flavor += 1) {
+      $("body").append("<h3>" + flavors[flavor] + "</h3>");
+  };
   var total = 1;
-  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  numbers.forEach(function(number) {
-    total *= number;
-  });
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  for (var index = 0; index < numbers.length; index += 1) {
+    total *= numbers[index];
+  };
   alert(total);
 });
